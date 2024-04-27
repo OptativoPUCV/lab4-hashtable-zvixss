bc7c71a722219b610b53ca11a8b139d9318142b2
zvixss <163350026+zvixss@users.noreply.github.com>
2024-04-27 20:19:59 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -67,7 +67,19 @@ void insertMap(HashMap * map, char * key, void * value)
 void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
 
+  Pair** aux = map -> buckets;
+  map -> capacity *= 2;
+  map -> buckets = (Pair**) malloc(sizeof(Pair*) * (map -> capacity * 2));
+  map -> size = 0;
 
+  for (int i = 0; i < map -> capacity; i++)
+    {
+      if (aux[i] != NULL)
+      {
+        insertMap(map, aux[i] -> key, aux[i] -> value);
+      }
+    }
+  free(aux);
 }
 
 
