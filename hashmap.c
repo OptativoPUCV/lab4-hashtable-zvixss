ce51d8a61f81d990769e0e47ccadc4be1fdc83c7
zvixss <163350026+zvixss@users.noreply.github.com>
2024-04-27 20:37:40 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -127,8 +127,21 @@ Pair * searchMap(HashMap * map,  char * key)
     return NULL;
 }
 
-Pair * firstMap(HashMap * map) {
+Pair * firstMap(HashMap * map) 
+{
+  if (map == NULL)
+  {
+    return NULL;
+  }
 
+  for (int i = 0; i < map -> capacity; i++)
+    {
+      if ( map -> buckets[i] != NULL  && map-> buckets[i] -> key == NULL)
+      {
+        map -> current = i;
+        return map -> buckets[i];
+      }
+    }
     return NULL;
 }
 
